6da7a3ac68873a8d60ccb9e205d3030a219a3bb6
coldwater007 <2030466960@qq.com>
2018-06-24 18:54:42 +0800

    update.....

---
+++ test_koa/DB_config/config.js
@@ -0,0 +1,10 @@
+var config = {
+    database: 'edu_system',
+    username: 'root',
+    password: '123456',
+    host: 'localhost',
+    port: 3306
+};
+//数据库链接相关配置信息
+
+module.exports = config;
--- test_koa/app.js
+++ test_koa/app.js
@@ -6,6 +6,39 @@ const controller = require('./controller');
 
 const app = new Koa();
 
+
+
+const Sequelize = require('sequelize');
+
+const config = require('./DB_config/config');
+
+
+console.log('init sequelize...');
+
+//建立对应数据库的  ORM
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+//建立一个 ORM 实体模型    学生登陆表
+var Load = sequelize.define('load', {
+    loadid: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    password: Sequelize.STRING(100)
+
+}, {
+    timestamps: false
+});
+
+
 // 打印请求信息
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
