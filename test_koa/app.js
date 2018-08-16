534d9b9c849f8554b39dc82c7cdbe20fbafa2c43
coldwater007 <2030466960@qq.com>
2018-08-16 16:36:15 +0800

    update.....

--- test_koa/app.js
+++ test_koa/app.js
@@ -10,6 +10,7 @@
  */
 
 const Koa = require('koa');
+const cors = require('koa-cors');
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 const app = new Koa();
@@ -18,6 +19,8 @@ app.use(async (ctx, next) => {
     console.log(`正在请求 ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 });
+app.use(cors()); //允许跨域
+
 app.use(bodyParser());//注册body解析功能
 app.use(controller());//调用controller.js中逻辑完成post get请求逻辑的注册
 app.listen(3000);//监听3000端口  开启服务
